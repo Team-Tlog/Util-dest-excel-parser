1b82117ea6646267fb43f0c6f08ffe0201253183
patternplane <bss011202@naver.com>
2025-09-07 17:46:37 +0900

    refact: 요청 파일 추출하는 로직을 함수로 분리

--- Util-dest-excel-parser/Util_dest_excel_parser/views.py
+++ Util-dest-excel-parser/Util_dest_excel_parser/views.py
@@ -54,25 +54,33 @@ def getServicePage():
 
 # ================ Service API ================
 
-@app.route('/parse', methods=['POST'])
-def parse_file():
+def getFile(req) :
+    """
+    요청의 파일을 반환합니다.
+    (file, errorMsg)
+    """
     # 요청에 파일이 포함되어 있는지 확인합니다.
-    if 'file' not in request.files:
-        return 'fail : file not found'
+    if 'file' not in req.files:
+        return (None, 'fail : file not found')
     
-    file = request.files['file']
+    file = req.files['file']
 
     # 파일 이름이 비어 있는지 확인합니다.
     if file.filename == '':
-        return 'fail : invalid name'
+        return (None, 'fail : invalid name')
     
     # 파일을 처리합니다.
-    if file:
-        return jsonify(service.readFile(file))
+    if not file:
+        return (None, 'fail : can not read')
     
-    return 'fail : can not read'
+    return (file, '')
 
 
-
+@app.route('/parse', methods=['POST'])
+def parse_file():
+    file, errorMsg = getFile(request)
+    if file :
+        return jsonify(service.readFile(file)), 200
+    return jsonify({'message' : errorMsg}), 500
 
 
